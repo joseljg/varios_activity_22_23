dddb0f22606b278f137d9136083a4f23b4acd452
joseljg <joseljg@gmail.com>
2022-10-07 17:50:08 +0200

    version 4: estados de un activity

--- app/src/main/java/es/joseljg/variosactivity/MainActivity.java
+++ app/src/main/java/es/joseljg/variosactivity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,21 +14,82 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_TEXTO_MENSAJE = "es.joseljg.variosactivity.MainActivity.mensaje_enviado";
     private static final int PETICION1 = 1;
+    public static final String EXTRA_NUMERO = "es.joseljg.variosactivity.MainActivity.numero_enviado";
+    public static final String EXTRA_TEXTO_MENSAJE2 = "es.joseljg.variosactivity.MainActivity.mensaje_enviado2";
     private EditText edt_texto_envio_pantalla1;
     private TextView txt_recibido_pantalla1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edt_texto_envio_pantalla1 = (EditText) findViewById(R.id.edt_texto_envio_pantalla1);
         txt_recibido_pantalla1 = (TextView) findViewById(R.id.txt_recibido_pantalla1);
+        Log.i("ciclo_de_vida", "estoy en el estado oncreate de la pantalla1");
     }
+//------------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclo_de_vida", "estoy en el estado onstart de la pantalla1");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclo_de_vida", "estoy en el estado onresume de la pantalla1");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclo_de_vida", "estoy en el estado onpause de la pantalla1");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclo_de_vida", "estoy en el estado onstop de la pantalla1");
+    }
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclo_de_vida", "estoy en el estado ondestroy de la pantalla1");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclo_de_vida", "estoy en el estado onrestart de la pantalla1");
+    }
+
+    //------------------------------------------------------------------------------------------------
 
     public void enviar_a_pantalla2(View view) {
         Intent intent = new Intent(this, Pantalla2Activity.class);
         String texto = String.valueOf(edt_texto_envio_pantalla1.getText());
-        intent.putExtra(EXTRA_TEXTO_MENSAJE,texto);
+        Bundle bundle = new Bundle();
+        bundle.putString(EXTRA_TEXTO_MENSAJE, texto);
+        bundle.putInt(EXTRA_NUMERO, 3);
+        bundle.putString(EXTRA_TEXTO_MENSAJE2, "hola");
+        intent.putExtras(bundle);
+       // intent.putExtra(EXTRA_TEXTO_MENSAJE,texto);
         startActivityForResult(intent,PETICION1);
     }
 
--- app/src/main/java/es/joseljg/variosactivity/Pantalla2Activity.java
+++ app/src/main/java/es/joseljg/variosactivity/Pantalla2Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,12 +28,18 @@ public class Pantalla2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla2);
         txt_recibido_pantalla2 = (TextView) findViewById(R.id.txt_recibido_pantalla2);
         edt_respuesta_pantalla2 = (EditText) findViewById(R.id.edt_respuesta_pantalla2);
+        Log.i("ciclo_de_vida", "estoy en el estado oncreate de la pantalla2");
 
         //-------------------------------------------------
         Intent intent = getIntent(); // enlazo con el intent recibido
         if(intent != null)
         {
-            String texto_recibido_en_el_intent =  intent.getStringExtra(MainActivity.EXTRA_TEXTO_MENSAJE);
+            Bundle bundle = intent.getExtras();
+            String texto_recibido_en_el_intent =  bundle.getString(MainActivity.EXTRA_TEXTO_MENSAJE);
+            String texto2 = bundle.getString(MainActivity.EXTRA_TEXTO_MENSAJE2);
+            int numero = bundle.getInt(MainActivity.EXTRA_NUMERO);
+
+            // String texto_recibido_en_el_intent =  intent.getStringExtra(MainActivity.EXTRA_TEXTO_MENSAJE);
             txt_recibido_pantalla2.setText(texto_recibido_en_el_intent);
         }
     }
@@ -43,6 +50,60 @@ public class Pantalla2Activity extends AppCompatActivity {
         Intent intent_respuesta = new Intent();
         intent_respuesta.putExtra(EXTRA_MENSAJE_RESPUESTA,texto_respuesta);
         setResult(RESULT_OK, intent_respuesta);
-        finish();
+       finish();
     }
+
+
+    //------------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("ciclo_de_vida", "estoy en el estado onstart de la pantalla2");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("ciclo_de_vida", "estoy en el estado onresume de la pantalla2");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("ciclo_de_vida", "estoy en el estado onpause de la pantalla2");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("ciclo_de_vida", "estoy en el estado onstop de la pantalla2");
+    }
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("ciclo_de_vida", "estoy en el estado ondestroy de la pantalla2");
+    }
+
+    //------------------------------------------------------------------------------------------------
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ciclo_de_vida", "estoy en el estado onrestart de la pantalla2");
+    }
+
+    //------------------------------------------------------------------------------------------------
 }
